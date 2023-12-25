61a55e66fbbd4278b22738876da68f10040eafb4
ttak0422 <ttak0422@gmail.com>
2023-12-25 10:05:01 +0000

    chore: impl Display for enums

--- bundler/src/constants.rs
+++ bundler/src/constants.rs
@@ -1,9 +1,37 @@
+use std::fmt::{self};
+
 #[derive(Debug, Clone, Default, PartialEq, Eq, PartialOrd, Ord)]
 pub enum Language {
     Vim,
     #[default]
     Lua,
     Fennel,
+}
+
+impl fmt::Display for Language {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match &self {
+            Language::Vim => write!(f, "vim"),
+            Language::Lua => write!(f, "lua"),
+            Language::Fennel => write!(f, "fennel"),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Default, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Target {
+    Vim,
+    #[default]
+    Neovim,
+}
+
+impl fmt::Display for Target {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match &self {
+            Target::Vim => write!(f, "vim"),
+            Target::Neovim => write!(f, "neovim"),
+        }
+    }
 }
 
 pub mod dir {
