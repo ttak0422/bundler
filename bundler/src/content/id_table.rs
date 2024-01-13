a3cf4275c49e9449b531b9f570c9fdcde69505a0
ttak0422 <ttak0422@gmail.com>
2024-01-13 15:35:50 +0000

    refactor: show error message

--- bundler/src/content/id_table.rs
+++ bundler/src/content/id_table.rs
@@ -12,6 +12,7 @@ impl IdTable {
     /// precondition: the key must be the value registered at initialization.
     pub fn get<T: TableKey + ?Sized>(&self, key: &T) -> &str {
         &self.value.get(key.key()).unwrap()
+        &self.value.get(key.key()).expect("unregistered key was used")
     }
 }
 
