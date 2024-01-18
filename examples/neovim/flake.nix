839e7aa3de619101982b8c734c96a6470379dcd2
ttak0422 <ttak0422@gmail.com>
2024-01-18 16:03:07 +0000

    chore: add comment to neovim template

--- examples/neovim/flake.nix
+++ examples/neovim/flake.nix
@@ -13,36 +13,76 @@
       systems = nixpkgs.lib.systems.flakeExposed;
 
       perSystem = { pkgs, ... }: {
+
+        # Please see the documentation for more information.
         bundler-nvim = {
+
+          # The configuration defined in `default` can be accessed as `bundler-nvim`.
           default = {
+
+            # Define viml code to be executed at startup.
+            extraConfig = ''
+              set laststatus=3
+            '';
+
+            # Define lua code to be executed at startup.
             extraLuaConfig = ''
               vim.loader.enable()
             '';
+
+            # Define plugins to be loaded at startup.
             eagerPlugins = with pkgs.vimPlugins; [{
               plugin = kanagawa-nvim;
+
+              # Define viml code to be executed at startup.
+              #
+              # following sample code is equivalent to
+              #
+              # ```nix
+              # startupConfig = {
+              #   language = "vim";
+              #   code = ''
+              #    colorscheme kanagawa
+              #   '';
+              # };
+              #
               startupConfig = ''
                 colorscheme kanagawa
               '';
             }];
+
+            # define plugins to be lazy loaded.
             lazyPlugins = with pkgs.vimPlugins; [{
               plugin = telescope-nvim;
-              startupConfig = {
-                language = "lua";
-                code = ''
-                  local builtin = require('telescope.builtin')
-                  vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-                  vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-                  vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-                  vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-                '';
-              };
+
+              # `startupConfig` is executed at startup, regardless of whether the plugin is loaded or not.
+              startupConfig = ''
+                nnoremap <leader>ff <cmd>Telescope find_files<cr>
+                nnoremap <leader>fg <cmd>Telescope live_grep<cr>
+                nnoremap <leader>fb <cmd>Telescope buffers<cr>
+                nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+              '';
+
+              # `preConfig` is executed before the plugin is loaded.
+              preConfig = "";
+
+              # `postConfig` is executed after the plugin is loaded.
               postConfig = {
+                # Explicit language specification is required when use lua code.
                 language = "lua";
                 code = ''
                   require('telescope').setup()
                 '';
               };
+
+              # Load the plugin when the following commands are executed.
+              onCommands = [ "Telescope" ];
             }];
+          };
+
+          # The configuration defined in `foo` can be accessed as `bundler-nvim-foo`.
+          foo = {
+            # ...
           };
         };
       };
