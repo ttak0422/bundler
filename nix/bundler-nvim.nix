d99c0c2f521a6b06d5913d057a249fffb6dda67e
ttak0422 <ttak0422@gmail.com>
2024-02-01 01:45:42 +0900

    chore: update patch version

--- nix/bundler-nvim.nix
+++ nix/bundler-nvim.nix
@@ -2,7 +2,7 @@
   bundler-nvim = {
     package = pkgs.vimUtils.buildVimPlugin {
       pname = "bundler-nvim";
-      version = "2.0.0";
+      version = "2.1.0";
       src = ./../bundler-nvim;
     };
   };
